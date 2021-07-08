1786144cba621de7ff344ff7bbde27e9e8183c01
Promise Morka <onyekamorka06@gmail.com>
2021-07-08 00:57:13 +0000

    Rewrote the function using arrow function and method chaining

--- script.js
+++ script.js
@@ -30,11 +30,17 @@ function calcAverageHumanAge(ages) {
   }, 0) / filterOutput.length;
   console.log(averageOfAllDogs);
 }
-
 calcAverageHumanAge([5,2,4,1,15,8,3]);
 
 
-// console.log(humanAgeOutput);
-// console.log(filterOutput);
-// console.log(averageOfAllDogs);
 
+// Rewrite the 'calcAverageHumanAge' function, but this time as an arrow function, and using chaining!
+// Test data:
+// § Data1:[5,2,4,1,15,8,3] § Data2:[16,6,10,5,6,1,4]
+
+function calcAverageHumanAge(ages) {
+  const humanAgeOutput = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4).filter(currentHumanAge => currentHumanAge >= 18).reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  console.log(humanAgeOutput);
+  
+}
+calcAverageHumanAge([5,2,4,1,15,8,3]);
